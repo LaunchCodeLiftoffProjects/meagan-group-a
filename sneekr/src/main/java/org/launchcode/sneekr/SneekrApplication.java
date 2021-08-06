2e5e9aa65252ebf03ab875001f5781cf277207a1
Jason <jwrounds@usfca.edu>
2021-08-06 14:12:51 -0500

    Spring boot project initialized

---
+++ sneekr/src/main/java/org/launchcode/sneekr/SneekrApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.sneekr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneekrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneekrApplication.class, args);
+	}
+
+}
