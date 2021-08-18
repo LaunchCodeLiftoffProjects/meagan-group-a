0aa2b7abc919628565ead8ea85ad60cc2f5c7433
Jason <jwrounds@usfca.edu>
2021-08-17 19:55:54 -0500

    Clarified h2 comments, fixed grammar in SQL table

--- sneekr/src/main/resources/data.sql
+++ sneekr/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO items (name, price) VALUES
     ('Air Jordan', 99.99),
-    ('Steel-toes Boots', 120.99),
+    ('Steel-toed Boots', 120.99),
     ('Clogs', 39.99),
     ('Sandals', 15.99);
