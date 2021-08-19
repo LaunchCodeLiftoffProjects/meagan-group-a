36726bacec8a2b44349135315cf630036bad8b1d
Jason <jwrounds@usfca.edu>
2021-08-19 12:50:53 -0500

    Added basic Item model, repository, and REST controller

---
+++ sneekr/src/main/java/org/launchcode/sneekr/models/Item.java
@@ -0,0 +1,83 @@
+package org.launchcode.sneekr.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private String description;
+    private double price;
+
+    public Item(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Item() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (id != item.id) return false;
+        if (Double.compare(item.price, price) != 0) return false;
+        if (!name.equals(item.name)) return false;
+        return description.equals(item.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.description, this.price);
+    }
+}
