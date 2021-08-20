0d57b18c7be06f2fe927f97f0f82141ed0847d77
Jason <jwrounds@usfca.edu>
2021-08-20 14:14:45 -0500

    Created basic cart model

---
+++ sneekr/src/main/java/org/launchcode/sneekr/models/Cart.java
@@ -0,0 +1,64 @@
+package org.launchcode.sneekr.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private List<Item> items = new ArrayList<>();
+
+    public Cart() {
+
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.items.remove(item);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder itemList = new StringBuilder();
+        for (Item i : this.items) {
+            itemList.append(i.toString()).append(" ");
+        }
+        return "Cart{" + "id=" + id + ", items=" + itemList + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cart cart = (Cart) o;
+
+        if (id != cart.id) return false;
+        return items.equals(cart.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.items);
+    }
+}
