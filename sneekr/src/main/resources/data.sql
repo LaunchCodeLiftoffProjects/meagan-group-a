d8c709c509ad3c6311fa5c1846f1944f5a08b78e
Demet Akcaoglu <demetakcaoglu@gmail.com>
2021-09-06 22:42:26 -0500

    Serverside spring security configurations

--- sneekr/src/main/resources/data.sql
+++ sneekr/src/main/resources/data.sql
@@ -3,3 +3,11 @@ INSERT INTO items (name, price) VALUES
     ('Steel-toed Boots', 120.99),
     ('Clogs', 39.99),
     ('Sandals', 15.99);
+
+insert into users(username, password,enabled) values
+    ('user','user',true),
+    ('admin','admin',true);
+
+insert into authorities(username, authority) values
+    ('user','ROLE_USER'),
+    ('admin','ROLE_ADMIN');
--- sneekr/src/main/resources/schema.sql
+++ sneekr/src/main/resources/schema.sql
@@ -5,3 +5,16 @@ CREATE TABLE items (
     name VARCHAR(250) NOT NULL,
     price DOUBLE NOT NULL
 );
+
+create table users(
+                      username varchar_ignorecase(50) not null primary key,
+                      password varchar_ignorecase(50) not null,
+                      enabled boolean not null
+);
+
+create table authorities (
+                             username varchar_ignorecase(50) not null,
+                             authority varchar_ignorecase(50) not null,
+                             constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username,authority);
