62ad091928a53dc110ca6f056c6eb068573a6bc3
Davey <daveyhwa@gmail.com>
2021-09-13 16:02:32 -0500

    added the itemDetail skeleton. Still can't ng s. TODO work on get and post in clone

---
+++ sneekr/src/main/webapp/src/app/item-detail/item-interface.ts
@@ -0,0 +1,10 @@
+export interface ItemDetails{
+    id: number
+    name: string;
+    photos: string[];
+    description: string;
+    sizes: string[]
+    colors: string[]
+    price: number
+    
+}
