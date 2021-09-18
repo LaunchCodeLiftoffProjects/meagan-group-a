0b3b81e8b749efa172fa8a9ad556a07d699b0344
Jason Rounds <jwrounds@usfca.edu>
2021-09-17 19:29:25 -0500

    Fixed app.module and add-to-cart path issues

--- sneekr/src/main/webapp/src/app/item-detail/add-to-cart/add-to-cart.component.ts
+++ sneekr/src/main/webapp/src/app/item-detail/add-to-cart/add-to-cart.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { testItem } from '../mock-item-details';
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  selector: 'app-add-to-cart',
+  templateUrl: './add-to-cart.component.html',
+  styleUrls: ['./add-to-cart.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class AddToCartComponent implements OnInit {
 
   itemDetails= testItem
   itemPrice = this.itemDetails.price
